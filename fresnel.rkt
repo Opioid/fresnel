2f4e428e563bd827d8e78ae9cad3ba97b6f75392
Opioid <benjamin.savs@gmail.com>
2019-07-27 12:35:42 +0200

    Using infix notation for improved readability of formulas

--- fresnel.rkt
+++ fresnel.rkt
@@ -1,44 +1,30 @@
-#lang racket
-
+#lang at-exp racket
+(require infix)
 (require plot)
 
-(define (pow5 x)
-  (let ([x2 (* x x)])
-    (* x2 x2 x)))
-
-(define (pow6 x)
-  (let ([x2 (* x x)])
-    (* x2 x2 x2)))
-
 (define (schlick-f0 n0 n1)
-  (let ([t (/ (- n0 n1) (+ n0 n1))])
-    (* t t)))
+  (let ([t @${(n0 - n1) / (n0 + n1)}])
+    @${t * t}))
 
-(define (schlick wo-dot-h f0)
-  (+ f0
-     (* (pow5 (- 1.0 wo-dot-h))
-        (- 1.0 f0))))
+(define (schlick costheta f0)
+  @${f0 + (1.0 - costheta)^5 * (1.0 - f0)})
 
 (define (degrees->cos x)
-  (- 1.0 (/ x 90.0)))
+  @${1.0 - x / 90.0})
 
 (define (lazanyi-schlick-a f0 f82)
-  (let ([cos-theta-max (/ 1.0 7.0)])
-    (/ (- (+ f0
-             (- 1.0
-                (* f0 (pow5 (- 1.0 cos-theta-max)))))
-          f82)
-       (* cos-theta-max
-          (pow6 (- 1.0 cos-theta-max))))))
+  (let ([costhetamax (/ 1.0 7.0)])
+    @${(f0 + (1.0 - f0) * (1.0 - costhetamax)^5 - f82)
+       /
+       (costhetamax * (1.0 - costhetamax)^6)}))
 
-(define (lazanyi-schlick wo-dot-h f0 a)
-  (- (schlick wo-dot-h f0)
-     (* a wo-dot-h (pow6 (- 1.0 wo-dot-h)))))
+(define (lazanyi-schlick costheta f0 a)
+ @${schlick[costheta, f0] - a * costheta * (1.0 - costheta)^6})
 
 (define ior 2.8)
 ;; (define f0 (schlick-f0 1.0 ior))
 (define f0 0.8)
-(define f82 1.0)
+(define f82 0.8)
 
 (define a (lazanyi-schlick-a f0 f82))
 
@@ -53,7 +39,8 @@ a
                  #:label "lazanyi-schlick"
                  #:color 1))
  #:y-min 0.0
- #:width 800
+ #:width 512
+ #:height 512
  #:legend-anchor 'bottom-left
  "fresnel.png")
 
